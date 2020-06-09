a2e1753bc73b296288c67822899ca33d8e8e1047
Markus Hofbauer <markus.hofbauer@tum.de>
2020-06-09 18:12:11 +0200

    init github actions with ros

--- install.sh
+++ install.sh
@@ -1,71 +1,47 @@
 #!/usr/bin/env bash
 
-usage() {
-    echo -e "Run: $0 [OPTIONS...] [OTHER ARGUMENTS]"
-    echo ""
-    echo "options:"
-    echo -e "\t--carla-root PATH : Absolute path to the CARLA root folder"
-}
+ROS_VERSION='melodic'
 
-if [[ $# -lt 1 ]]; then
-    usage
-    exit 1
-fi
+# Create a log file of the installation process as well as displaying it
+exec > >(tee telecarla-install.log)
+exec 2>&1
 
-if [[ $1 == "-h" || $1 == "--help" ]]; then
-    usage
-    exit 0
-elif [[ $1 == "--carla-root" && $# -gt 1 ]]; then
-    CARLA_ROOT=$2
-    ROS_VERSION='melodic'
+echo 'Setup TeleCarla'
 
-    # Create a log file of the installation process as well as displaying it
-    exec > >(tee telecarla-install.log)
-    exec 2>&1
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WS_SRC="${SCRIPT_DIR}"/..
 
-    echo 'Setup TeleCarla'
+pushd "$WS_SRC" || exit
 
-    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    WS_SRC="${SCRIPT_DIR}"/..
+# General Packages
+sudo apt-get update
+sudo apt-get install -y \
+    software-properties-common \
+    apt-utils \
+    python-pip \
+    python3-pip \
+    python-protobuf
 
-    pushd "$WS_SRC" || exit
+# Carla Ros Bridge
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 81061A1A042F527D
+sudo add-apt-repository "deb [arch=amd64 trusted=yes] http://dist.carla.org/carla-ros-bridge-melodic/ bionic main"
+sudo apt-get update
+sudo apt-get install -y carla-ros-bridge-${ROS_VERSION}
 
-    # General Packages
-    sudo apt-get update
-    sudo apt-get install -y \
-        software-properties-common \
-        apt-utils \
-        python-pip \
-        python3-pip \
-        python-protobuf
+# Carla Scenario Runner
+git clone https://github.com/carla-simulator/scenario_runner.git
+pip2 install -r scenario_runner/requirements.txt
+pip3 install -r scenario_runner/requirements.txt
 
-    # Carla Ros Bridge
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 81061A1A042F527D
-    sudo add-apt-repository "deb [arch=amd64 trusted=yes] http://dist.carla.org/carla-ros-bridge-melodic/ bionic main"
-    sudo apt-get update
-    sudo apt-get install -y carla-ros-bridge-${ROS_VERSION}
+# Tele Carla GUI
+"$SCRIPT_DIR"/telecarla_gui/script/install_dependencies.sh
 
-    # Carla Scenario Runner
-    git clone https://github.com/carla-simulator/scenario_runner.git
-    pip2 install -r scenario_runner/requirements.txt
-    pip3 install -r scenario_runner/requirements.txt
-    bash scenario_runner/setup_environment.sh --carla-root "$CARLA_ROOT"
+# Tele Carla RPC
+"$SCRIPT_DIR"/telecarla_rpc/script/install.sh
 
-    # Tele Carla GUI
-    "$SCRIPT_DIR"/telecarla_gui/script/install_dependencies.sh
+# GStreaming
+"$SCRIPT_DIR"/gstreaming/setup/install_gstreamer.sh
 
-    # Tele Carla RPC
-    "$SCRIPT_DIR"/telecarla_rpc/script/install.sh
-
-    # GStreaming
-    "$SCRIPT_DIR"/gstreaming/setup/install_gstreamer.sh
-
-    # Final Notes
-    echo "You should add the following commands to your shell config"
-    echo "source /opt/carla-ros-bridge/$ROS_VERSION/setup.bash"
-
-    exit 0
-else
-    usage
-    exit 1
-fi
+# Final Notes
+echo "You should add the following commands to your shell config"
+echo "source /opt/carla-ros-bridge/$ROS_VERSION/setup.bash"
