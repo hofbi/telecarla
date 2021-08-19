9057c359ce865d5f1ca726c6c17b72ff4d3b3d32
Markus Hofbauer <markus.hofbauer@tum.de>
2021-08-19 14:48:31 +0200

    refactor rpc com lib (#22)

--- telecarla_rpc/source/src/telecarla_rpc_server.cpp
+++ telecarla_rpc/source/src/telecarla_rpc_server.cpp
@@ -18,15 +18,15 @@ TeleCarlaRpcServer::TeleCarlaRpcServer(ros::NodeHandle& nh, ros::NodeHandle& pnh
     const auto prefix = "/carla/" + pnh.param("role_name", std::string("ego_vehicle"));
 
     server_.bind("set_enable_autopilot",
-                 common::RPCMsgCallback<data::Bool>(nh.advertise<std_msgs::Bool>(prefix + "/enable_autopilot", 1)));
+                 com::RPCMsgCallback<data::Bool>(nh.advertise<std_msgs::Bool>(prefix + "/enable_autopilot", 1)));
     ROS_INFO_STREAM("Provide RPC call set_enable_autopilot -> " << prefix << "/enable_autopilot");
-    server_.bind("set_vehicle_control_manual_override",
-                 common::RPCMsgCallback<data::Bool>(
-                     nh.advertise<std_msgs::Bool>(prefix + "/vehicle_control_manual_override", 1)));
+    server_.bind(
+        "set_vehicle_control_manual_override",
+        com::RPCMsgCallback<data::Bool>(nh.advertise<std_msgs::Bool>(prefix + "/vehicle_control_manual_override", 1)));
     ROS_INFO_STREAM("Provide RPC call set_vehicle_control_manual_override -> " << prefix
                                                                                << "/vehicle_control_manual_override");
     server_.bind("set_vehicle_control_cmd_manual",
-                 common::RPCMsgCallback<data::ControlCommands>(
+                 com::RPCMsgCallback<data::ControlCommands>(
                      nh.advertise<carla_msgs::CarlaEgoVehicleControl>(prefix + "/vehicle_control_cmd_manual", 1)));
     ROS_INFO_STREAM("Provide RPC call set_vehicle_control_cmd_manual -> " << prefix << "/vehicle_control_cmd_manual");
 
